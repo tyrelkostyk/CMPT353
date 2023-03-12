38a9a91b458cbf931991718add00297ac8686418
tyrelkostyk <tck290@mail.usask.ca>
2023-03-12 15:54:47 -0600

    cleaned up & modified my test app - I'm getting it!

--- a4copy/frontend/src/App.js
+++ a4copy/frontend/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+		const newCount = count + 1;
+		setCount(newCount);
+	}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          Your very own react App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		<button onClick={handleClick}>
+			{count}
+		</button>
       </header>
     </div>
   );
