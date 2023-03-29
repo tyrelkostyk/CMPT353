97914466a234be3470e11c3e9f51835473e574b4
tyrelkostyk <tck290@mail.usask.ca>
2023-03-28 19:42:25 -0600

    got part A partially done: created DBs, inserting new posts

--- a5/serverA.js
+++ a5/serverA.js
@@ -1,11 +1,10 @@
 // Tyrel Kostyk
 // tck290, 11216033
-// CMPT353 - Assignment 4
-// March 15 2023
+// CMPT353 - Assignment 5
+// March 28 2023
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const path = require('path').join(__dirname, 'frontend', 'build');
 const cors = require('cors');
 const nano = require('nano')(process.env.COUCHDB_URL);
 
@@ -16,126 +15,89 @@ const host = '0.0.0.0';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(express.static(path));
 app.use(cors());
 
-const databaseName = "db";
-const db = nano.use(databaseName);
+const postsDbName = "posts_database";
+const commentsDbName = "comments_database";
+const postsDb = nano.use(postsDbName);
+const commentsDb = nano.use(commentsDbName);
 
-const tableName = "posts";
+// create the couchdb databases if it doesn't exist
+async function initDb() {
+	try {
+		// get list of all existing databases
+		const dbList = await nano.db.list();
 
-// Part A -> initialize a DB and table
-// TODO: replace this API with a function that checks if cdb is initialized
-let serverInitialized = false;
-app.get('/api/init', (req, res) => {
-
-	// prevent multiple connection attempts
-	if (serverInitialized) {
-		console.log("Server DB already initialized.");
-		res.json({ answer: "Success!" });
-		return;
-	}
-
-	connection.connect((err) => {
-		if (err) {
-			console.error("Failed to connect to database: ", err);
-			res.status(500).send("Error connecting to database.");
-		}
-		console.log("Connected to MySql DB ");
-	});
-
-	// create the database
-	const createQuery = `CREATE DATABASE IF NOT EXISTS ${databaseName}`;
-	connection.query(createQuery, (err) => {
-		if (err) {
-			console.error("Failed to create database: ", err);
-			res.status(500).send("Error creating database.");
-		}
-		console.log("MySql DB (" + databaseName + ") created successfully");
-
-		const useQuery = `USE ${databaseName}`;
-		connection.query(useQuery, (err) => {
-			if (err) {
-				console.error("Failed to use database: ", err);
-				res.status(500).send("Error using database.");
-			}
-			console.log("Using " + databaseName + " database");
-
-			// drop the table (if it exists)
-			const dropQuery = `DROP TABLE IF EXISTS ${tableName}`;
-			connection.query(dropQuery, (err) => {
-				if (err) {
-					console.error("Failed to drop table: ", err);
-		            res.status(500).send("Error dropping table.");
-				}
-				console.log("Dropped " + tableName + " table");
-			});
-
-			// create table named posts
-			const createTableQuery = `CREATE TABLE IF NOT EXISTS ${tableName} (
-				id		INT UNSIGNED NOT NULL auto_increment,
-				topic	VARCHAR(100) NOT NULL,
-				data	VARCHAR(100) NOT NULL,
-				PRIMARY KEY (id)
-			)`;
-			connection.query(createTableQuery, (err) => {
-				if (err) {
-					console.error("Failed to create table: ", err);
-		            res.status(500).send("Error creating table.");
-				} else {
-					console.log("Created " + tableName + " table");
-					serverInitialized = true;
-					res.json({ answer: "Success!" });
-				}
-			});
-		});
-	});
-});
-
-
-// Part B -> Add a new posted message to DB
-app.post('/api/addPost', (req, res) => {
-    const topic = req.body.topic;
-    const data = req.body.data;
-
-	console.log('POST Request Received! Topic: ' + topic + ' Data: ' + data);
-
-	// insert into table
-	const insertQuery = `INSERT INTO ${tableName} (topic, data)
-						VALUES ('${topic}', '${data}') `;
-	connection.query(insertQuery, (err, result) => {
-		if (err) {
-            console.error("Failed to insert post: ", err);
-            res.status(500).send("Error inserting post into the database.");
-        } else {
-            console.log("Inserted " + topic + ":" + data);
-            res.json({ answer: "wrote successfully!" });
-        }
-	})
-});
-
-
-// Part B -> GET method that returns all the posted DB entries
-app.get('/api/getPosts', (req, res) => {
-	// select from table
-	const selectQuery = `SELECT * FROM ${tableName}`;
-	connection.query(selectQuery, (err, result) => {
-		if (err) {
-			console.log("getPosts failed: ", err);
-			res.status(500).send("Error querying the database.");
+		// create posts db if it doesn't exist
+		if (!dbList.includes(postsDbName)) {
+			await nano.db.create(postsDbName);
+			console.log(`${postsDbName} created!`);
 		} else {
-			const response = JSON.parse(JSON.stringify(result));
-			console.log("Retrieved: ", response);
-			res.json(response);
+			console.log(`${postsDbName} already exists; continuing...`);
 		}
-	});
+
+		// create comments db if it doesn't exist
+		if (!dbList.includes(commentsDbName)) {
+			await nano.db.create(commentsDbName);
+			console.log(`${commentsDbName} created!`);
+		} else {
+			console.log(`${commentsDbName} already exists; continuing...`);
+		}
+
+	} catch (error) {
+		console.error('Error creating databases: ', error);
+	}
+}
+
+initDb();
+
+
+// main page entry point
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/TestRestLevel2.html')
 });
 
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) => {
-  res.sendFile('index.html', {path});
+// Add a new post to the posts DB
+app.post('/addPost', async (req, res) => {
+    const { postId, postTopic, postData } = req.body;
+	const post = {
+		_id: postId,
+		topic: postTopic,
+		data: postData
+	};
+
+	console.log('POST Received! ID: ' + postId +
+					       ' Topic: ' + postTopic +
+						    ' Data: ' + postData);
+
+	try {
+		const result = await postsDb.insert(post);
+		console.log('Inserted ' + postId + '@' + postTopic + ':' + postData);
+		// res.json(result);
+		res.json({ answer: 'wrote successfully!' });
+	} catch (error) {
+		console.error('Failed to insert post: ', error);
+		res.status(500).json({ asnwer: 'Error inserting post into the database.'});
+	}
 });
+
+
+// // GET method that returns all the posted DB entries
+// app.get('/api/getPosts', (req, res) => {
+// 	// select from table
+// 	const selectQuery = `SELECT * FROM ${tableName}`;
+// 	connection.query(selectQuery, (err, result) => {
+// 		if (err) {
+// 			console.log("getPosts failed: ", err);
+// 			res.status(500).send("Error querying the database.");
+// 		} else {
+// 			const response = JSON.parse(JSON.stringify(result));
+// 			console.log("Retrieved: ", response);
+// 			res.json(response);
+// 		}
+// 	});
+// });
 
 // make the app listen
 app.listen(port, host, () => {
